82ea230e8a5c9a8a0d8e1752330dbe84fa82de1f
Muhammad Nur Hidayat <dayaters22@gmail.com>
2020-10-01 07:57:20 +0800

    Feat: Install dependecies & set config

--- src/main.js
+++ src/main.js
@@ -2,6 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import titleMixin from './mixins/title_mixin'
+import VueConfirmDialog from 'vue-confirm-dialog'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.mixin(titleMixin)
+Vue.use(VueConfirmDialog)
+Vue.component('vue-confirm-dialog', VueConfirmDialog.default)
+
 Vue.config.productionTip = false
 
 new Vue({
