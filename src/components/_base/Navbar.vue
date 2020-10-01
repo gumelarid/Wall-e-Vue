e3148367c6c1a0ec9c3ac0d98cdd31ea03998de6
Muhammad Nur Hidayat <dayaters22@gmail.com>
2020-10-01 20:28:19 +0800

    Create notification display

--- src/components/_base/Navbar.vue
+++ src/components/_base/Navbar.vue
@@ -23,9 +23,32 @@
                   </div>
                 </div>
                 <div class="notification">
-                 <div class="notif-icon">
-                   <b-icon icon="bell"></b-icon>
-                 </div>
+                  <div class="notif-icon">
+                    <div>
+                      <b-dropdown variant="link" right toggle-class="text-decoration-none" no-caret>
+                        <template v-slot:button-content>
+                          <b-icon icon="bell"></b-icon>
+                        </template>
+                        <b-dropdown-item href="#">
+                          <b-icon icon="arrow-down"></b-icon>
+                          <div class="notif-detail">
+                            <span class="notif-subject">Transfered from Joshua Lee</span><br />
+                          <span class="notif-pricing">Rp 22.000</span>
+                          </div>
+                        </b-dropdown-item>
+                        <b-dropdown-item href="#">
+                          <b-icon icon="arrow-up"></b-icon>
+                          <div class="notif-detail">
+                            <span class="notif-subject">Transfer to Jessica Lee</span><br />
+                          <span class="notif-pricing">Rp 22.000</span>
+                          </div>
+                        </b-dropdown-item>
+                        <div align="center">
+                          <router-link to="/transfer">See All</router-link>
+                        </div>
+                      </b-dropdown>
+                    </div>
+                  </div>
                 </div>
               </div>
             </b-col>
@@ -35,5 +58,4 @@
     </header>
   </div>
 </template>
-
-<style src="../../assets/style/walle_style.css" scoped></style>
+<style src="../../assets/style/walle_style.css"></style>
