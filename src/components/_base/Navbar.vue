b89545462eb4a7abaeacbc07322658659081e544
Muhammad Nur Hidayat <dayaters22@gmail.com>
2020-10-01 10:49:05 +0800

    Templating page - Navbar

---
+++ src/components/_base/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <header>
+      <nav class="walle-navbar">
+        <b-container>
+          <b-row>
+            <b-col md="5" class="nav-title">
+              <router-link to="/dashboard">
+                <span>Wall-e</span>
+              </router-link>
+            </b-col>
+            <b-col md="6" class="nav-profile">
+              <div class="profile-wrapper">
+                <div class="profile-image">
+                  <img src="../../assets/selena-gomez.jpg" alt="">
+                </div>
+                <div class="profile-info">
+                  <div class="profile-name">
+                    <span>Selena Gomez</span>
+                  </div>
+                  <div class="profile-phone">
+                    <span>+62 8139 3877 7946</span>
+                  </div>
+                </div>
+                <div class="notification">
+                 <div class="notif-icon">
+                   <b-icon icon="bell"></b-icon>
+                 </div>
+                </div>
+              </div>
+            </b-col>
+          </b-row>
+        </b-container>
+      </nav>
+    </header>
+  </div>
+</template>
---
+++ src/views/Template_Page.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Navbar />
+  </div>
+</template>
+<script>
+import Navbar from '../components/_base/Navbar.vue'
+
+export default {
+  title: 'Page Name | Wall-e',
+  name: 'Template',
+  components: {
+    Navbar
+  }
+}
+
+</script>
+
+<style src="../assets/style/walle_style.css"></style>
