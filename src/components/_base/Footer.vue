2a43acf142566b9c8b1dda8e3228013393b0aade
Dhea <dheaaqn@gmail.com>
2020-10-06 20:05:01 +0700

    update footer

--- src/components/_base/Footer.vue
+++ src/components/_base/Footer.vue
@@ -5,13 +5,13 @@
         <b-container>
           <b-row>
             <b-col md="6" class="footer-address">
-              <span>2020 Zwallet. All right reserved.</span>
+              <span>2020 Walle. All right reserved.</span>
             </b-col>
             <b-col md="3" class="footer-phone">
               <span>+62 5637 8882 9901</span>
             </b-col>
             <b-col md="3" class="footer-email">
-              <span>contact@zwallet.com</span>
+              <span>wallecorps@gmail.com</span>
             </b-col>
           </b-row>
         </b-container>
